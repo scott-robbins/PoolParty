1048352850575a3aae4c3f0d1a3cf849a395afe4
TylersDurden <srobbin1@ithaca.edu>
2019-07-29 20:50:39 -0400

    last touchups on initial design for now

--- code/client.py
+++ code/client.py
@@ -32,12 +32,57 @@ _________ - (3) Machine Types - __________
 
 
 if 'init' in sys.argv:
+    Master = IP
+    Delegators = ['192.168.1.229']
+    Workers = ['192.168.1.217', '192.168.1.200']
     for host in peers:
         uname = names[host]
         pw = utils.retrieve_credentials(host)
         comp_bench = 'cd ~/Desktop/RAXion/code; python compbench.py'
-        result = utils.ssh_command(host, uname, pw, comp_bench, False)
-        print result
+        utils.ssh_command(host, uname, pw, comp_bench, False)
+        utils.ssh_command(host, uname, pw, 'ping -c 1 1.1.1.1', False)
+        # TODO Use Results to Determine Machines [2] and rest [3]
+    for filename in os.listdir(os.getcwd()):
+        if 'ion' in filename.split('.'):
+            print '*\033[3m Configuration Model Found \033[0m\033[1m< %s >\033[0m' % filename
+            # LOAD CONFIGURATION DATA
+            raw_config = {'Master':'', 'Schedulers': [], 'Workers': [],
+                          'Programs': []}
+            for line in utils.swap(filename, False):
+                try:
+                    raw_config['Master'] = line.split('[1]: ')[1]    # Master Machine
+                except IndexError:
+                    pass
+                try:
+                    raw_config['Schedulers'] = line.split('[2]: ')[1].split(',')[:]    # Scheduler Machine(s)
+                except IndexError:
+                    pass
+                try:
+                    raw_config['Workers'] =line.split('[3]: ')[1].split(',')[:]    # Worker Machine(s)
+                except IndexError:
+                    pass
+                try:
+                    prog1 = line.split('\t<1>')[1].replace(' ','')    # Program 1?
+                    raw_config['Programs'].append(prog1)
+                except IndexError:
+                    pass
+                try:
+                    prog2 = line.split('\t<2>')[1].replace(' ','')    # Program 1?
+                    raw_config['Programs'].append(prog2)
+                except IndexError:
+                    pass
+                try:
+                    prog3 = line.split('\t<3>')[1].replace(' ','')    # Program 1?
+                    raw_config['Programs'].append(prog3)
+                except IndexError:
+                    pass
+                
+            print '\nWorkers: %s' % raw_config['Workers']
+            print 'Scheduler(s): %s' % raw_config['Schedulers']
+            print 'Master Machine: %s' % raw_config['Master']
+            print 'Programs In ION:'
+            for prog in raw_config['Programs']:
+                print '* %s ' % prog
 
 if 'update_all' in sys.argv:
     update_all(True)
