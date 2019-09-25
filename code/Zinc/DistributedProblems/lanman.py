c9e62dd40ed4b0c531946b62a5332a1dd0fe4985
TylersDurden <srobbin1@ithaca.edu>
2019-09-25 08:42:03 -0400

    having read/write lock issue I think

--- code/Zinc/DistributedProblems/lanman.py
+++ code/Zinc/DistributedProblems/lanman.py
@@ -9,7 +9,7 @@ import os
 # To minimize extra outputs to console you can add exceptions here and 
 # They won't be displayed as part of the rogue IP detection/tracking
 exceptions = ['localhost', 'Fios_Quantum_Gateway', 'fios-router', os.uname()[1]]
-
+is_root = False
 
 def create_logfile(verbose):
     date, localtime = utils.create_timestamp()
@@ -17,15 +17,18 @@ def create_logfile(verbose):
     log_date = date.split('/')[0]+date.split('/')[1]+date.split('/')[2]
     log_time = localtime.split(':')[0]+localtime.split(':')[1] + localtime.split(':')[2]
     log_name = 'LOG_' + log_date + '_' + log_time + '.txt'
-    open(log_name,'w').write(log_data+'\n')
+
     if verbose:
         print '\033[1m'+log_data+'\033[0m'
     return log_name, date, localtime
 
 
 def start_nx_listener(iface, logfile):
-    cmd = 'sudo tcpdump -i %s >> %s' % (iface, logfile)
-    print 'Executing: %s' % cmd
+    if is_root:
+        cmd = 'tcpdump -i %s >> %s' % (iface, logfile)
+    else:
+        cmd = 'sudo tcpdump -i %s >> %s' % (iface, logfile)
+    print 'Executing: %s' %cmd
     os.system(cmd)
 
 
@@ -67,18 +70,20 @@ def run(iface, duration, logfile, verbose):
     tic = time.time()
     conxs = {}
 
-    ''' Start the Packet Capture Parser Thread BEFORE network listener'''
-    pool = ThreadPool(processes=1)
-
     '''  Start the Network Listener  '''
     p = Thread(target=start_nx_listener, args=(iface, logfile))
-    p.daemon = True
+    p.setDaemon(True)
+    # p.daemon = True
     p.start()
+
+    ''' Start the Packet Capture Parser Thread AFTER network listener'''
+    pool = ThreadPool(processes=1)
 
     while (time.time() - tic) <= duration:
         dt = time.time() - tic
         try:
-            if dt > 0 and dt % 5 == 0:
+            if dt > 0 and dt % 5 == 0 and p.isAlive():
+                # TODO: Having issues with nx_listener thread locking up !
                 async_read = pool.apply_async(parse_logs, (logfile, verbose))
                 conxs, ARPs = async_read.get()
         except KeyboardInterrupt:
@@ -106,22 +111,13 @@ def dump_traffic(traffic):
     return unknown_hosts
 
 
-def discover_niface():
-    cmd = 'ls /sys/class/net/ | while read n; do ifconfig | grep $n: >> ifaces.txt; done'
-    os.system(cmd)
-    iface = ''.replace(' ', '')
-    for line in utils.swap('ifaces.txt', True):
-        if ('RUNNING' in line.split(',')) and ('BROADCAST' in line.split(',')):
-            iface = line.split(':')[0]
-    return iface
-
-
 if __name__ == '__main__':
     DEBUG = False
     log, date, localtime = create_logfile(DEBUG)
-
-    iface = discover_niface()
-    traffic, arps = run(iface, 10, log, DEBUG)
+    if os.getuid() == 0:
+        is_root = True
+    iface = utils.discover_niface()
+    traffic = run(iface, 10, log, DEBUG)
 
     ''' Show Summary of Monitoring Session'''
     os.system('clear')
