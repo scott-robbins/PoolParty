029fc6a4ac5c58af0093b7d6fde8b3961a5e0f8c
TylersDurden <srobbin1@ithaca.edu>
2019-11-23 20:08:42 -0500

    Making new design seperate from previous work

--- code/Lynx/conductive.py
+++ code/Lynx/conductive.py
@@ -27,12 +27,11 @@ def ephemeral_keygen(verbose):
     return pbk
 
 
-def send_public_key(pbk, peer, port):
-    reply = ''
-    if os.path.isfile('tmp.pem'):
+def send_key(pbk, peer, port):
+    if os.path.isfile('key.pem'):
         data = ''
-        for line in utils.swap('tmp.pem',False):
-            data += line
+        for line in utils.swap('key.pem',False):
+            data += line + '\n'
     else:
         print '[!!] No Public Key to Send.'
         return -2
@@ -91,24 +90,38 @@ def recv_remote_keyfile(port, timeout):
 
 
 def create_secure_handshake(peer):
-    secret = list(get_random_bytes(8))
+    secured = False
+    content = 'HELLO'
+    for element in list(get_random_bytes(8)):
+        content += element
+    pbkey = utils.load_key('key.pem')
+    # Use RSA key to encrypt secret and send it
+    secret = pbkey.encrypt(content, 1)
+    pbkey.decrypt(secret)
     # Determine Known Peers and see if given peer is known
     local_files = os.listdir(os.getcwd())
-    if peer in utils.prs or (peer.replace('.','')+'.txt') in local_files:
+    if peer in utils.prs or (peer.replace('.','')+'.pem') in local_files:
         print '[*] %s is a known Peer' % peer
     # If so make connection
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((peer, 41414))
-    except socket.error:
-        print '[!!] Failed to Connect to %s' % peer
-        return 0
-    # Else exchange public keys
-    # Use public key to encrypt secret and send it
+    while not secured:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((peer, 41414))
+            s.send(secret[0])
+            reply = s.recv(1024)
+            if reply == content:
+                print '[*] Secure Handshake Success [1/2]'
+                s.send(pbkey.encrypt('SUCCESS', 2)[0])
+                s.close()
+                secured = True
+        except socket.error:
+            print '[!!] Connect \033[1mBROKEN\033[0m with %s' % peer
+            return 0
+
+    ''' ^^ OUTBOUND TRAFFIC TO PEER HAS BEEN SECURED [1/2] ^^'''
     # Other end will do the same (you receive their encrypted secret)
-    # Both will use each others public key to decrypt nonce secret
-    # reply with the decrypted answers
-    # Append the actually messages to the replies, and if the secret is correct
+    # Use each others keys to decrypt nonce secret and reply w. decrypted answers
+    # Append the actual messages to replies, and if the secret is correct
     # Read the message (cut down on # of cnx/packets total!)
 
 
@@ -117,11 +130,19 @@ def secret_listener(timeout):
     tic = time.time()
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind('0.0.0.0', 41414)
+        s.bind(('0.0.0.0', 41414))
         s.listen(5)
         while time.time() - tic < timeout:
             client, addr = s.accept()
-
+            print '[*] %s Has Connected' % addr[0]
+            query = client.recv(1024)
+            result, client = decode_secret(query, client)
+            print '[*] Query Decoded into:\n %s' % result
+            client.send(result)
+            ackd, client = decode_secret(client.recv(1024), client)
+            if 'SUCCESS' in ackd.split('SUCCESS')[0]:
+                print ackd
+                client.send('TRUE')
     except socket:
         s.close()
         print 'Connection Broken!'
@@ -129,20 +150,33 @@ def secret_listener(timeout):
     s.close()
 
 
+def decode_secret(data_in, peer):
+    ip = peer.getpeername()[0]
+    keyfilename = ip.replace('.', '') + '.pem'
+    print 'Decoding with %s' % keyfilename
+    rmt_pbk = utils.load_key(keyfilename)
+    reply = rmt_pbk.decrypt(data_in)
+    return reply, peer
+
+
 '''                 ______<(|[ MAIN ]|)>______                '''
+verb = True
 if 'keygen' in sys.argv:
-    public_key = ephemeral_keygen(False)
+    public_key = ephemeral_keygen(verbose=verb)
 
 if 'send_pub_key' in sys.argv:
-    send_public_key(ephemeral_keygen(False), sys.argv[2], 6666)
+    send_key(ephemeral_keygen(False), sys.argv[2], 6666)
 
 if 'rcv_key' in sys.argv:
     recv_peers_key(6666, 5)
 
-if 'hello' in sys.argv:
+if 'listen' in sys.argv:
     secret_listener(30)
 
+if 'hello' in sys.argv:
+    rmt = sys.argv[2]
+    create_secure_handshake(rmt)
 
-create_secure_handshake('192.168.1.200')
+# create_secure_handshake('192.168.1.200')
 
 # EOF
