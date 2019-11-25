e6f48b3e9ac54d012a88609e8dfd3a8ff4a6c654
TylersDurden <srobbin1@ithaca.edu>
2019-11-24 20:29:11 -0500

    updates

---
+++ code/Lynx/api.py
@@ -0,0 +1,39 @@
+from Crypto.Random import get_random_bytes
+from Crypto.Hash import SHA256
+from Crypto.Cipher import AES
+import base64
+import socket
+import utils
+import time
+import sys
+import os
+
+
+class API:
+    commands = {}
+    remote = ''
+    port = 0
+    session_token = ''
+
+    def __init__(self, rmt, port):
+        self.remote = rmt
+        self.port = port
+        self.session_token = self.get_token()
+
+    def get_token(self):
+        value = ''
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((self.remote, self.port))
+            s.send('?')
+            value = s.recv(256)
+            s.close()
+        except socket.error:
+            print '[!!] Connection Error'
+            s.close()
+            pass
+        return value
+
+
+# Example
+agent = API('192.236.160.95', 12345)
--- code/Lynx/lynx.py
+++ code/Lynx/lynx.py
@@ -1,168 +1,77 @@
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
-from multiprocessing import Pool
-import matplotlib.pyplot as plt
-import Tkinter as Tk
-import tkFileDialog
+from Crypto.Random import get_random_bytes
+from Crypto.Hash import SHA256
+from Crypto.Cipher import AES
+import base64
 import socket
 import utils
 import time
 import sys
 import os
 
-root = Tk.Tk()
-w = 1200;   h = 800
-base = Tk.Canvas(root, height=h, width=w)
-base.pack()
+# the block size for the cipher object; must be 16 per FIPS-197
+BLOCK_SIZE = 16
+PADDING = '{'
+pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING        # pad the text to be encrypted
+EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))            # encrypt with AES, encode with base64
+DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
 
-NAMES = []; PEERS = []; buttons = []
-
-
-def query_lan(i):
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(.25)
-        s.connect((i, 22))
-        addr = i
-        print '\033[1m\033[31m[*] \033[0m\033[1m%s Discovered\033[0m' % i
-        s.close()
-        return addr
-    except socket.error:
-        pass
-        s.close()
-    return ''
+date, localtime = utils.create_timestamp()
 
 
-def discover_nodes():
-    p = Pool(processes=10)
-    queued = ''
-    addrs = []
-    self = utils.get_local_ip()
+class TestServer:
+    clients_seen = []
+    actions = {'?': base64.b64encode(get_random_bytes(128))}
+    inbound_port = 12345
+    outgoin_port = 12346
+    tic = 0.0
 
-    for i in range(1, 254):
-        ip = '192.168.1.%d' % i
-        live = p.apply_async(query_lan, (ip,)).get(timeout=1)
-        if live != self and len(live)>0:
-            addrs.append(live)
-    return addrs
+    def __init__(self):
+        self.init()
 
+    def init(self,):
+        # Make sure you have the correct permissions
+        if os.getuid() != 0:
+            print '[!!] Not running as root'
+            exit()
+        self.running = True
+        try:
+            uptime = self.run()
+        except KeyboardInterrupt:
+            print '[*] Killing Server!'
+            self.running = False
+            # TODO: Dump unlogged data
+            exit()
 
-def add_clock():
-    # Get Date and Time for Clock Display
-    date, localtime = utils.create_timestamp()
-    timestamp = date + '\t' + localtime
-    base.create_rectangle(425, 0, 730, 35, fill='#2200bf')
-    base.create_text(575, 15, text=timestamp, font=('Papyrus', 12), fill='white')
+    def run(self):
+        print '[*] Server Started %s - %s [*]' % (date, localtime)
+        self.tic = time.time()
+        while self.running:
+            try:
+                '''                  Socket Setup                 '''
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.bind(('0.0.0.0', self.inbound_port))
+            except socket.error:
+                print 'Cannot Assign bind socket'
+                s.close()
+                time.sleep(10)
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.bind(('0.0.0.0', self.inbound_port))
 
+            s.listen(5)
+            '''                  Client/Event Handling        '''
+            client_sock, addr = s.accept()
+            print '[*] Connection Accepted from %s:%d' % (addr[0], addr[1])
+            client_query = client_sock.recv(1024)
+            if client_query in self.actions.keys():
+                reply = self.actions[client_query]
+                print '[$] %s => %s' % (client_query, reply)
+                client_sock.send(reply)
+                client_sock.close()
+            s.close()
+        return time.time()-self.tic
 
-def add_menu(fcns):
-    # Add a File/Help Menubar
-    menu = Tk.Menu(root)
-    root.config(menu=menu)
-    filemenu = Tk.Menu(menu)
-    menu.add_cascade(label="File", menu=filemenu)
-    for label in fcns.keys():
-        filemenu.add_command(label=label, command=fcns[label])
-        filemenu.add_separator()
-    filemenu.add_command(label="Exit", command=root.quit)
-    helpmenu = Tk.Menu(menu)
-    menu.add_cascade(label="Help", menu=helpmenu)
-
-
-def choose_file():
-    root.filename = tkFileDialog.askopenfilename(initialdir=os.getcwd(), title="Select file",
-                                                 filetypes=(("jpeg files", "*.jpg"),
-                                                            ("python code", "*.py"),
-                                                            ("all files", "*.*")))
-    return root.filename
-
-
-def send_file():
-    target_file = choose_file()
-    form = Tk.Toplevel(root)
-    Tk.Label(form, text="Enter Peer Name: ").pack()
-    e = Tk.Entry(form)
-    e.pack(padx=5)
-
-    def submit():
-        addr = e.get()
-        print 'Sending %s %s' % (addr, target_file)
-
-        form.destroy()
-
-    b = Tk.Button(form, text="Send", command=submit)
-    b.pack(pady=5)
+    def get_uptime(self):
+        return time.time()-self.tic
 
 
-def node_event(event):
-    cx = event.x
-    cy = event.y
-    item = base.find_closest(event.x, event.y)
-
-
-def add_live_nodes():
-    tic = time.time()
-    global buttons, PEERS, NAMES
-    PEERS = discover_nodes()
-    colors = ['green', '#00ff00']
-    index = 0
-    for node_handle in PEERS:
-        x1 = 10
-        y1 = index * 110 + 5
-        x2 = x1 + 100
-        y2 = y1 + 50
-        node_button = base.create_rectangle(x1, y1, x2, y2, fill=colors[0], tags="clickable")
-        node_title = base.create_text(x1 + 50, y1 + 20, text=node_handle, font=("Papyrus", 10), fill='black')
-        base.bind(node_button, '<Button-1>', node_event)
-        buttons.append(node_button)
-        index += 1
-
-    base.bind('<Button-1>', node_event)
-    print 'Nodes Linked [%ss Elapsed]' % str(time.time()-tic)
-
-
-class AddToPool:
-    def __init__(self, parent):
-        top = self.top = Tk.Toplevel(parent)
-        self.a = Tk.Label(top, text="Host Name").grid(row=0, column=0)
-        self.b = Tk.Label(top, text="IP Address").grid(row=1, column=0)
-        self.c = Tk.Label(top, text="Password").grid(row=2, column=0)
-        self.a1 = Tk.Entry(top);    self.b1 = Tk.Entry(top);    self.c1 = Tk.Entry(top)
-        # Place the forms fields into canvas
-        self.a1.grid(row=0, column=1);  self.b1.grid(row=1, column=1);  self.c1.grid(row=2, column=1)
-        # add submit button
-        b = Tk.Button(top, text="Submit", command=self.write)
-        b.grid(row=4, column=1)
-
-    def write(self):
-        global NAMES
-        global PEERS
-        host = self.a1.get()
-        ip = self.b1.get()
-        pw = self.c1.get()
-        self.top.destroy()
-
-        if ip not in NAMES.keys():
-            print 'Adding %s@%s to nodes' % (host, ip)
-            host_file = ip.replace('.', '') + '.txt'
-            hkey_file = ip.replace('.', '') + '.key'
-            os.system('python aes.py -e %s' % pw)
-            os.system('mv encrypted.txt KEYS/%s' % host_file)
-            os.system('mv key.txt KEYS/%s' % hkey_file)
-            NAMES.keys().append(host)
-            NAMES[host] = ip
-            PEERS.append(ip)
-            # check_nodes()
-        else:
-            print '[!!] %s is ALREADY a Peer' % ip
-
-
-add_live_nodes()
-
-
-add_menu({'Open': choose_file,
-          'Send': send_file})
-add_clock()
-Tk.mainloop()
-# EOF
-
+TestServer()
