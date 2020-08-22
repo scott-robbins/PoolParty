9e7775c41c14d2b5b2730faa3137ded1e2e081b1
TylersDurden <srobbin1@ithaca.edu>
2020-08-22 17:02:44 -0400

    starting to develop the node approach

--- code/0.3/node.py
+++ code/0.3/node.py
@@ -23,7 +23,7 @@ class Node:
 
 
 	def __init__(self, nickname):
-		self.external_ip = utils.get_ext_ip()
+		self.external_ip = utils.get_ext_ip().replace('$','').replace(' ','')
 		self.get_internal_addr()
 		print self.external_ip
 		for addr in self.internal_ip.values():
