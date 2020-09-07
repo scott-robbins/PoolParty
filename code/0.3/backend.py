836a1193f0a7271160e4d4358ac2c24cd2e88831
TylersDurden <srobbin1@ithaca.edu>
2020-09-07 07:52:40 -0400

    Need a multithreaded messaging channel

--- code/0.3/backend.py
+++ code/0.3/backend.py
@@ -60,13 +60,11 @@ class BackendListener:
   		# API_DAT MUST REQUIRE NODES NAME TO LOAD CORRECT PUBLIC KEY FOR ENCRYPTION 
   		peer = raw_req.split(' :::: ')[1].split(' ;;;; ')[0].replace(' ','')
   		sess_id = '%s@%s' % (peer, ci[0])
-  		# k = base64.b64encode(get_random_bytes(32))
-  		if sess_id not in self.session_keys.keys():
-  			print '[*] Assinging %s a new Session ID' % peer
-  			self.session_keys[sess_id] =  base64.b64encode(get_random_bytes(16))
+  		k = base64.b64encode(get_random_bytes(16))
+  		# self.session_keys[sess_id] =  base64.b64encode(get_random_bytes(16))
   		n, i, pw, pk = control.load_credentials(peer, False)
   		# cipher_rsa = PKCS1_OAEP.new(pk.publickey()).encrypt(k)
-		enc_session_key = PKCS1_OAEP.new(pk.publickey()).encrypt(self.session_keys[sess_id])
+		enc_session_key = PKCS1_OAEP.new(pk.publickey()).encrypt(k)
 		c.send(enc_session_key)
   		# TODO: ADD ENCRYPTION TO API REQUESTS!!!!
   		if api_req in self.actions.keys():
