3177dcf80ad3ad680e16bfc378604d8745665421
TylersDurden <srobbin1@ithaca.edu>
2020-09-06 20:56:57 -0400

    Need a multithreaded messaging channel

--- code/0.3/backend.py
+++ code/0.3/backend.py
@@ -59,7 +59,7 @@ class BackendListener:
   		# API_DAT MUST REQUIRE NODES NAME TO LOAD CORRECT PUBLIC KEY FOR ENCRYPTION 
   		peer = raw_req.split(' :::: ')[1].split(' ;;;; ')[0].replace(' ','')
   		sess_id = '%s@%s' % (peer, ci[0])
-  		if sess_id not in self.session_keys.keys():
+  		if sess_id in self.session_keys.keys():
   			self.session_keys[sess_id] = base64.b64encode(get_random_bytes(32))
   		try:
   			n, i, pw, pk = control.load_credentials(peer, False)
