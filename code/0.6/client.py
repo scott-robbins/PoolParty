ece8c6f26c8eb3bb707caa11d973dfb87aac14c6
TylersDurden <0ptik41@protonmail.com>
2020-12-19 01:16:10 -0500

    fixing some small bugs introduced while refactoring

--- code/0.6/client.py
+++ code/0.6/client.py
@@ -40,7 +40,7 @@ def update_all(peers):
 
 def dump_peers(node, peers):
 	result = ''
-	ip,m,hname,connected = get_creds(node, peers)
+	ip,m,hname,pw,connected = get_creds(node, peers)
 	if connected:
 		try:
 			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -52,13 +52,13 @@ def dump_peers(node, peers):
 			print('[!!] Connection Error')
 			return ''
 	else:
-		print('[!!} Peer %s is not online' % node)
+		print('[!!] Peer %s is not online' % node)
 		return ''
 	return result
 
 def dump_shares(node, peers):
 	result = ''
-	ip,m,hname,connected = get_creds(node, peers)
+	ip,mac,hname,pword,connected = get_creds(node, peers)
 	if connected:
 		try:
 			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -70,25 +70,29 @@ def dump_shares(node, peers):
 			print('[!!] Connection Error')
 			return ''
 	else:
-		print('[!!} Peer %s is not online' % node)
+		print('[!!] Peer %s is not online' % node)
 		return ''
 	return result	
 
-def get_creds(n,pool):
+def get_creds(n,peers):
 	# maybe be able to handle ip or a hostname
-	if node in peers.keys():
-		p = peers[node]
+	if n in peers.keys():
+		p = peers[n]
 	else:
 		found = False
 		for n in peers.keys():
 			# print(peers[n])
-			if node in peers[n].values():
+			if n in peers[n].values():
 				found = True
 				p = peers[n]
 				break
+		if os.path.isfile(os.getcwd()+'/'+n):
+			h, i, p, m = utils.load_credentials(n.split('/')[-1].split('.')[0],False)
+			p = {'hname':h,'ip':i,'pword':p,'mac':m,'connected':True}
+			found = True
 		if not found:
-			print('[!!] cannot find %s' % node)
-			return ''
+			print('[!!] cannot find %s' % n)
+			return '','','','',''
 	
 	ip = p['ip']
 	mac = p['mac']
@@ -99,26 +103,34 @@ def get_creds(n,pool):
 
 
 def main():
+	verbose = True
+	peers = []
 	if os.path.isfile('.env'):
 		h,e,i,s = setup.load_local_vars()
 
-	peers = setup.test_connections(True)
-	print('[*] %d Peers Connected' % count_connected(peers))
+		peers = setup.test_connections(True)
+		print('[*] %d Peers Connected' % count_connected(peers))
 
 
 	if '--cmd-all' in sys.argv:
-		replies = cmd_all(peers, utils.arr2chr(sys.argv[2:]))
-		exit()
+		if len(peers):
+			replies = cmd_all(peers, utils.arr2chr(sys.argv[2:]))
+			exit()
+		else:
+			print('[!!] No peers to command (need creds)')
 
 	if '--update' in sys.argv:
 		update_all(peers)
 		exit()
 
 	if '--check-peers' in sys.argv and len(sys.argv) > 1:
-		print(dump_peers(sys.argv[2], peers))
-
+		rps = dump_peers(sys.argv[2], peers)
+		if verbose:
+			print(rps)
 	if '--check-shares' in sys.argv and len(sys.argv) > 2:
-		print(dump_shares(sys.argv[2], peers))
+		shares = dump_shares(sys.argv[2], peers)
+		if verbose:
+			print(shares)
 
 
 
